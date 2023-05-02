37b8939fe8f17b9c4f11d22b7cd80ef5207689a1
Anusha Ravulakollu <anusha.ravulakollu@gmail.com>
2023-05-02 11:39:08 +0530

    Using the Express Router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
-// const { json } = require('express/lib/response');
-
-// const req = require('express/lib/request');
-// const res = require('express/lib/response');
+const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -13,31 +11,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
+// const accountData = fs.readFileSync(
+//     path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+// );
 
-const accounts = JSON.parse(accountData);
+// const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(
-    path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
+// const userData = fs.readFileSync(
+//     path.join(__dirname, 'json', 'users.json'), 'utf8'
+// );
+// const users = JSON.parse(userData);
 
 app.get('/', (req, res) => { 
-    res.render('index', { title: 'Account Summary', accounts: accounts });
+    res.render('index', { title: 'Account Summary', accounts });
 });
 
-app.get('/savings', (req, res) => { 
-    res.render('account', { account: accounts.savings });
-});
-app.get('/checking', (req, res) => { 
-    res.render('account', { account: accounts.checking });
-});
-app.get('/credit', (req, res) => { 
-    res.render('account', { account: accounts.credit });
-});
+app.use('/account', accountRoutes);
+app.use('/services', serviceRoutes);
 
 app.get('/profile', (req, res) => { 
     res.render('profile', { user: users[0] });
